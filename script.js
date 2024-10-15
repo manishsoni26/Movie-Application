a3d0c7edb5f263b1b060836efb1daf60b669ccd3
Manish Soni <161626371+manishsoni26@users.noreply.github.com>
2024-10-15 21:45:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -59,39 +59,3 @@ form.addEventListener('submit',(e)=>{
         window.location.reload();
     }
 });
-
-
-
-
-
-// const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e60196be91358ae862773b990c7fa4c5&page=1';
-// const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-// const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=e60196be91358ae862773b990c7fa4c5&query=';
-
-// const form = document.getElementById('form');
-// const search = document.getElementById('search');
-
-// // Get initial movies
-// getMovies(API_URL);
-
-// async function getMovies(url) {
-//     try {
-//         const res = await fetch(url);
-//         const data = await res.json();
-//         console.log(data.results);  // Successfully fetched data
-//     } catch (error) {
-//         console.error('Error fetching the movies:', error);  // Error handling
-//     }
-// }
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const searchTerm = search.value.trim(); // Trim to remove excess spaces
-
-//     if (searchTerm && searchTerm !== '') {
-//         getMovies(SEARCH_API + encodeURIComponent(searchTerm)); // Encode search term
-//         search.value = '';
-//     } else {
-//         window.location.reload();
-//     }
-// });
